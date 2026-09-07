7f543f3a5801f2cfc1721f742f2f24514b2126c0
Rainbow-113 <nguyeenvanddan113@gmail.com>
2026-09-07 23:36:24 +0700

    merge code admin

--- app/lib/features/home/presentation/screens/home_wrapper_screen.dart
+++ app/lib/features/home/presentation/screens/home_wrapper_screen.dart
@@ -1,6 +1,7 @@
 import 'package:app/features/auth/data/models/user_models.dart';
 import 'package:app/features/home/presentation/screens/doctor_home_screen.dart';
 import 'package:app/features/home/presentation/screens/patient_home_screen.dart';
+import 'package:app/screens/admin/admin_dashboard.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -19,6 +20,8 @@ class _HomeWrapperScreen extends State<HomeWrapperScreen> {
       return const PatientHomeScreen();
     } else if (widget.role == UserRole.doctor) {
       return const DoctorHomeScreen();
+    } else if (widget.role == UserRole.admin) {
+      return const AdminDashboard();
     } else {
       return const Scaffold(body: Center(child: Text('Lỗi không có role')));
     }
