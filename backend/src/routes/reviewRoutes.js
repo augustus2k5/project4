cd80973b142b66e3e84ead10296ae544d5b96e5a
Hoàng Hải <hoanghai03112k5@gmail.com>
2026-09-08 21:11:41 +0700

    test 1.7 api

--- backend/src/routes/reviewRoutes.js
+++ backend/src/routes/reviewRoutes.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { createReview, getDoctorReviews } = require('../controllers/reviewController');
 
-router.post('/', createReview);                  
-router.get('/doctor/:doctorId', getDoctorReviews);  
+const { 
+  createReview, 
+  getDoctorReviews 
+} = require('../controllers/reviewController');
+
+const { verifyToken, authorizeRoles } = require('../middleware/authMiddleware');
+
+router.post('/', verifyToken, authorizeRoles('PATIENT'), createReview);
+
+router.get('/doctor/:doctorId', getDoctorReviews);
 
 module.exports = router;
