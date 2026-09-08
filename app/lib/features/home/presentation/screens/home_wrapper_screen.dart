28aaafecdcfcb770c1e59a976a7941198508c081
Hoàng Hải <hoanghai03112k5@gmail.com>
2026-09-08 09:52:46 +0700

    contemporary fix

--- app/lib/features/home/presentation/screens/home_wrapper_screen.dart
+++ app/lib/features/home/presentation/screens/home_wrapper_screen.dart
@@ -1,29 +1,29 @@
-import 'package:app/features/auth/data/models/user_models.dart';
-import 'package:app/features/home/presentation/screens/doctor_home_screen.dart';
-import 'package:app/features/home/presentation/screens/patient_home_screen.dart';
-import 'package:app/screens/admin/admin_dashboard.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+// import 'package:app/features/auth/data/models/user_models.dart';
+// import 'package:app/features/home/presentation/screens/doctor_home_screen.dart';
+// import 'package:app/features/home/presentation/screens/patient_home_screen.dart';
+// import 'package:app/screens/admin/admin_dashboard.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
 
-class HomeWrapperScreen extends StatefulWidget {
-  final UserRole role;
+// class HomeWrapperScreen extends StatefulWidget {
+//   final UserRole role;
 
-  const HomeWrapperScreen({super.key, required this.role});
-  @override
-  State<StatefulWidget> createState() => _HomeWrapperScreen();
-}
+//   const HomeWrapperScreen({super.key, required this.role});
+//   @override
+//   State<StatefulWidget> createState() => _HomeWrapperScreen();
+// }
 
-class _HomeWrapperScreen extends State<HomeWrapperScreen> {
-  @override
-  Widget build(BuildContext context) {
-    if (widget.role == UserRole.patient) {
-      return const PatientHomeScreen();
-    } else if (widget.role == UserRole.doctor) {
-      return const DoctorHomeScreen();
-    } else if (widget.role == UserRole.admin) {
-      return const AdminDashboard();
-    } else {
-      return const Scaffold(body: Center(child: Text('Lỗi không có role')));
-    }
-  }
-}
+// class _HomeWrapperScreen extends State<HomeWrapperScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     if (widget.role == UserRole.patient) {
+//       return const PatientHomeScreen();
+//     } else if (widget.role == UserRole.doctor) {
+//       return const DoctorHomeScreen();
+//     } else if (widget.role == UserRole.admin) {
+//       return const AdminDashboard();
+//     } else {
+//       return const Scaffold(body: Center(child: Text('Lỗi không có role')));
+//     }
+//   }
+// }
