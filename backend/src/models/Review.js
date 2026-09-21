4014a8f9903bef54b2e337a58dd9ffbeada8b710
Rainbow-113 <nguyeenvanddan113@gmail.com>
2026-09-22 02:11:59 +0700

    edit backend review

--- backend/src/models/Review.js
+++ backend/src/models/Review.js
@@ -4,26 +4,34 @@ const reviewSchema = new mongoose.Schema({
   appointmentId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Appointment',
-    required: true,
-    unique: true 
+    default: null
   },
   patientId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-    required: true
+     default: null
   },
+  customAuthorName: {
+        type: String,
+        default: '',
+        trim: true,
+      },
   doctorId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Doctor',
-    required: true
+    required: [true, 'Vui lòng cung cấp ID bác sĩ']
   },
   rating: { 
     type: Number, 
-    required: true, 
+    required: [true, 'Vui lòng chọn số sao đánh giá'],
     min: 1, 
     max: 5 
   }, 
-  comment: { type: String, default: '' } 
+  comment: { type: String, default: '' },
+  images: [{type: String}],
+  isAnonymous: {type: Boolean,default: false},
+  adminReply: {comment: { type: String, default: '' },repliedAt: { type: Date, default: null }}
 }, { timestamps: true });
-
+// Đảm bảo 1 lịch hẹn chỉ có 1 review (chỉ áp dụng khi có appointmentId)
+reviewSchema.index({ appointmentId: 1 }, { unique: true, sparse: true });
 module.exports = mongoose.model('Review', reviewSchema);
--- backend/src/routes/reviewRoutes.js
+++ backend/src/routes/reviewRoutes.js
@@ -3,13 +3,34 @@ const router = express.Router();
 
 const { 
   createReview, 
-  getDoctorReviews 
+  updateReview,
+  deleteReview,
+  getDoctorReviews,
+  getAppointmentReview,
+  adminGetAllReviews,
+  adminCreateReview,
+  adminUpdateReview,
+  adminDeleteReview,
+  adminReplyReview
+
 } = require('../controllers/reviewController');
 
 const { verifyToken, authorizeRoles } = require('../middleware/authMiddleware');
-
-router.post('/', verifyToken, authorizeRoles('PATIENT'), createReview);
-
 router.get('/doctor/:doctorId', getDoctorReviews);
+router.get('/appointment/:appointmentId', verifyToken, getAppointmentReview);
+router.post('/', verifyToken, authorizeRoles('PATIENT'), createReview);
+router.put('/:id', verifyToken, authorizeRoles('PATIENT'), updateReview);
+router.delete('/:id', verifyToken, authorizeRoles('PATIENT'), deleteReview);
+//admin
+router.get('/admin/all', verifyToken, authorizeRoles('ADMIN'), adminGetAllReviews);
+router.post('/admin', verifyToken, authorizeRoles('ADMIN'), adminCreateReview);
+router.put('/admin/:id', verifyToken, authorizeRoles('ADMIN'), adminUpdateReview);
+router.delete('/admin/:id', verifyToken, authorizeRoles('ADMIN'), adminDeleteReview);
+router.post('/admin/:id/reply', verifyToken, authorizeRoles('ADMIN'), adminReplyReview);
+
+
+
+
+
 
 module.exports = router;
