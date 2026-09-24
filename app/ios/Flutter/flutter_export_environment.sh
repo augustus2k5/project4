8b61c02e127ef13451c79be24165cfa60cce8828
Hoàng Hải <hoanghai03112k5@gmail.com>
2026-09-24 20:45:02 +0700

    Feat: Complete medical record creation and view history for doctor and patient

--- app/ios/Flutter/flutter_export_environment.sh
+++ app/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Projects\project4\app"
-export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=D:\Projects\project4\app\ios\Flutter\ephemeral\Packages\.packages\FlutterFramework"
+export "FLUTTER_ROOT=C:\dev\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\lala\Desktop\project 4\app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
